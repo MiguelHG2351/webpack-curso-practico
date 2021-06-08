b7b7af1d48358e193639b51542957c704e60c0f5
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-06-07 18:26:02 -0600

    configuración del puerto en backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
 import { Request, Response } from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const initalState = {
     "initalState": {
